a07b8bfb6061af785a85f09ab6fc97bc0727b7c8
trackingtrail <amoghnath23@gmail.com>
2020-12-30 19:35:50 +0530

    Update bulkrenameclitesting.py

--- bulkrenameclitesting.py
+++ bulkrenameclitesting.py
@@ -1,16 +1,21 @@
-import os, glob, sys
+import os
 import datetime 
+import re
 
 class colors:
     OKGREEN = '\033[92m'
     MAGENTA = '\033[95m'
     RED = '\033[31m'
     ENDC = '\033[0m'
+    YELLOW  = "\033[93m"
 
 print(colors.MAGENTA+'[~] Specify the format you want to rename the files in: '+colors.ENDC)
-print(colors.OKGREEN+'[/]'+colors.ENDC+' 1.Number-Text-Oldname '+colors.RED+'(e.g. 1.file.txt, 2.file.txt ...)'+colors.ENDC) 
-print(colors.OKGREEN+'[/]'+colors.ENDC+' 2.Number-Only '+colors.RED+'(e.g. 1., 2. ...)'+colors.ENDC)
-print(colors.OKGREEN+'[/]'+colors.ENDC+' 3.Date '+colors.RED+'(e.g. 2020-12-24, 2012-17-21 ...)\n'+colors.ENDC)
+print(colors.OKGREEN+'[/]'+colors.ENDC+' 1.Number-Text-Oldname '+colors.YELLOW+'(e.g. 1.file.txt, 2.file.txt ...)'+colors.ENDC) 
+print(colors.OKGREEN+'[/]'+colors.ENDC+' 2.Number-Only '+colors.YELLOW+'(e.g. 1., 2. ...)'+colors.ENDC)
+print(colors.OKGREEN+'[/]'+colors.ENDC+' 3.Date '+colors.YELLOW+'(e.g. 2020-12-24, 2012-17-21 ...)'+colors.ENDC)
+print(colors.OKGREEN+'[/]'+colors.ENDC+' 4.UPPERCASE '+colors.YELLOW+'(e.g. ROCKYOU.TXT, HELLO.JPG ...)'+colors.ENDC)
+print(colors.OKGREEN+'[/]'+colors.ENDC+' 5.lowercase '+colors.YELLOW+'(e.g. rockyou.txt, hello.jpg ...)\n'+colors.ENDC)
+
 user_rename_type = int(input('Format: '))
 
 global path, listfolders_path, renamed_file, source_file, filename
@@ -27,15 +32,24 @@ if os.path.isdir(path):
         elif user_rename_type == 2:
             renamed_file_numbering = path+renamed_file_numbering
             os.rename(source_file, renamed_file_numbering)
-
-    if user_rename_type == 3:
-        file_names = os.listdir(path)
-        for file_name in file_names:
-            old_file_path = os.path.join(path, file_name)
+            
+    file_names = os.listdir(path)
+    for file_name in file_names:
+        old_file_path = os.path.join(path, file_name)
+        if user_rename_type == 3:
             date_taken =  datetime.datetime.today().strftime ('%d-%b-%Y')
-            new_file_name = date_taken + file_name
+            new_file_name = date_taken+" "+file_name
             new_file_path = os.path.join(path, new_file_name)
             os.rename(old_file_path, new_file_path)
+        if user_rename_type == 4:
+            new_file_name = file_name.upper()
+            new_file_path = os.path.join(path, new_file_name)
+            os.rename(old_file_path, new_file_path)
+        if user_rename_type == 5:
+            new_file_name = file_name.lower()
+            new_file_path = os.path.join(path, new_file_name)
+            os.rename(old_file_path, new_file_path)
+
 else:
     print("Not a valid directory")
     exit()
